f6e9d08d848e465155ad38b62468f7966f807aff
Adam Christie <fractos@gmail.com>
2016-02-27 20:22:24 +0000

    explicitly using LoopProcessContext in LoopBehaviour to simplify access to the parent context.

--- Inversion.Extensibility/Process/Behaviour/LoopBehaviour.cs
+++ Inversion.Extensibility/Process/Behaviour/LoopBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using Inversion.Extensibility.Extensions;
 
 namespace Inversion.Process.Behaviour
@@ -13,9 +14,12 @@ namespace Inversion.Process.Behaviour
             _loop = loop;
         }
 
-        protected virtual IProcessContext ConfigureLoop(IProcessContext context)
+        protected virtual LoopProcessContext ConfigureLoop(IProcessContext context)
         {
-            IProcessContext loopContext = new LoopProcessContext(context, context.Services, context.Resources);
+            // create a loop context which has the list of behaviours registered and a
+            // copy of all parameters and control state objects.
+
+            LoopProcessContext loopContext = new LoopProcessContext(context, context.Services, context.Resources);
 
             loopContext.Register(_loop);
 
@@ -32,18 +36,26 @@ namespace Inversion.Process.Behaviour
             return loopContext;
         }
 
-        protected abstract IEnumerable<bool> LoopCondition(IEvent ev, IProcessContext loopContext);
+        protected abstract IEnumerable<bool> LoopCondition(IEvent ev, LoopProcessContext loopContext);
+
+        protected virtual void Finish(IEvent ev, LoopProcessContext loopContext) { }
 
         public override void Action(IEvent ev, IProcessContext context)
         {
-            int yieldTimeMs = Convert.ToInt32(this.Configuration.GetNameWithAssert("config", "yieldtimems"));
+            // get optional yield time to wait between iterations
+            int yieldTimeMs = this.Configuration.Has("config", "yieldtimems")
+                ? Convert.ToInt32(this.Configuration.GetNameWithAssert("config", "yieldtimems"))
+                : 0;
+
+            // get a defaulted message to fire on the loop context on each iteration
             string message = this.Configuration.GetOverride("loop-message", "work");
 
             // create a context and copy params and current control state into it
-            IProcessContext loopContext = this.ConfigureLoop(context);
+            LoopProcessContext loopContext = this.ConfigureLoop(context);
 
             bool firstTime = true;
 
+            // get our enumerator from the custom condition method
             using (IEnumerator<bool> enumerator = this.LoopCondition(ev, loopContext).GetEnumerator())
             {
                 do
@@ -56,8 +68,7 @@ namespace Inversion.Process.Behaviour
                     }
 
                     // only yield if we've been through the cycle before
-                    // don't want to arbitrarily yield after firing the event
-                    if (!firstTime)
+                    if (!firstTime && yieldTimeMs > 0)
                     {
                         System.Threading.Thread.Sleep(yieldTimeMs);
                     }
@@ -67,6 +78,9 @@ namespace Inversion.Process.Behaviour
                     
                 } while (enumerator.Current);
             }
+
+            // call optional custom code at the end of all iterations
+            this.Finish(ev, loopContext);
         }
     }
 }
